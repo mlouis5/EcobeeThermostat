b59df6ff81ef5b70db9ac620c24174ab586679b6
mlouis5 <macdersonlouis@gmail.com>
2015-06-19 08:04:17 -0400

    Initial Commit

---
+++ src/main/java/com/design/perpetual/ecobeethermostat/app/components/RuntimeSensorMetaData.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.design.perpetual.ecobeethermostat.app.components;
+
+/**
+ *
+ * @author Mac
+ */
+public class RuntimeSensorMetaData {
+    
+}
